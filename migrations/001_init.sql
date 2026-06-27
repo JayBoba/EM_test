656b134825bfd2c7ff87d8051374fe96191f4fb3
JayBoba <shimattaku@gmail.com>
2026-06-28 02:08:29 +0300

    migrations done, gonna do the db connection now

---
+++ migrations/001_init.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXIST "pgcrypto";
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id UUID PRIMARY KEY DEFAULT get_random_uuid(),
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL CHECK (price >= 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id ON subscriptons(user_id);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_service_name ON subscriptions(service_name);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_dates ON subscriptions(start_date, end_date);
