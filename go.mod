98b484636a0324f01829a5f0a3d58b3f6791c9fe
JayBoba <shimattaku@gmail.com>
2026-06-28 01:57:52 +0300

    model is done, migrations next

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/JayBoba/EM_test
 go 1.24.6
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/google/uuid v1.6.0
--- internal/models/sub.go
+++ internal/models/sub.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Subscription struct {
+	Id          uuid.UUID  `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	ServiceName string     `gorm:"not null" json:"service_name"`
+	Price       int        `gorm:"not null;check:price >= 0" json:"price"`
+	UserID      uuid.UUID  `gorm:"type:uuid;not null" json:"user_id"`
+	StartDate   time.Time  `gorm:"type:date;not null" json:"start_date"`
+	EndDate     *time.Time `gorm:"type:date" json:"end_date,omitempty"`
+}
